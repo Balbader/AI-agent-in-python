04258b672f72c12b0e7772b4a889ba1ee1159250
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-22 16:27:14 +0200

    add an optional command line flag, --verbose, that will allow us to toggle "verbose" output on and off

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ messages = [
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=messages
 )
-
 print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    print("User prompt: ", user_prompt)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
