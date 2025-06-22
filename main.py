6c23a8b3212fd0c4731e2e0a850774f6bb7970ef
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-22 16:21:16 +0200

    update code to store a list of messages in the conversation, and pass in the "role" appropriately.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -8,9 +10,18 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+user_prompt = sys.argv[1]
+
+if len(sys.argv) < 2:
+    print("Usage: python main.py <message>")
+    sys.exit(1)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place\
-        to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=messages
 )
 
 print(response.text)
