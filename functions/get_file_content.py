1b5a81a31983919ddadf7835f565d103a72fb572
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-23 18:13:57 +0200

    add function to get the contents of a file

---
+++ functions/get_file_content.py
@@ -0,0 +1,32 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        # Convert to absolute paths for proper comparison
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(file_path)
+
+        # if the file is outside of the working_directory, raise an error
+        if not abs_file_path.startswith(abs_working_dir):
+            return (f"Error: Cannot read '{file_path}' because it is outside\
+                    of the permitted working directory")
+
+        # if the file is not a file, raise an error
+        if not os.path.isfile(abs_file_path):
+            return (f"Error: File not found or is not a regular file:\
+                    '{file_path}'")
+
+        # Read the file content
+        with open(abs_file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        # Truncate if longer than 10000 characters
+        if len(content) > 10000:
+            content = (content[:10000] + f'\n[...File "{file_path}"\
+                    \ntruncated at 10000 characters...]')
+
+        return content
+
+    except Exception as e:
+        return f"Error: {str(e)}"
